1c313a6bcb3621575ea8a2046aafd4198efde26a
Eric <eric@saick.net>
2016-02-25 10:15:51 +0800

    Add supervisor usage

--- Node.js开发指南/app.js
+++ Node.js开发指南/app.js
@@ -6,6 +6,13 @@ var http = require("http");
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Node.js</h1>');
+  res.write('<h2>test</h2>');
   res.end('<p>Hello World</p>');
 }).listen(3000);
 console.log("Http server is listening at port 3000");
+
+/*
+Node.js开发起来容易，但是调试相对麻烦，需要重新启动服务。
+有supervisor可以解决此问题。
+`npm install -g supervisor`, `supervisor app.js`
+ */
